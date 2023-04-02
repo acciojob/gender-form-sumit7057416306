c9a3b917af8b7cde69c964827319d81791297419
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:33:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-
 import React from "react";
 import './../styles/App.css';
+import GenderForm from '../GenderForm.js'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <GenderForm />
     </div>
   )
 }
